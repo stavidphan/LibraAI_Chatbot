5ec978e70683966d21b4fe1eece98868d3d17eb9
StavidPhan <thanhduyphan2123@gmail.com>
2025-04-27 22:41:30 +0700

    update ngrok url

--- script.js
+++ script.js
@@ -124,7 +124,7 @@ document.addEventListener("DOMContentLoaded", function () {
         try {
             const startTime = performance.now();
     
-            const response = await fetch("https://01ac-171-241-76-223.ngrok-free.app/query", requestOptions);
+            const response = await fetch("https://5ce0-2405-4803-fe4f-fe0-eea-ad6-886c-27ec.ngrok-free.app/query", requestOptions);
             const result = await response.json();
     
             const endTime = performance.now();
